bd8e89c1f2dcc05d2d95c2011965e34a78d6b583
YellowFlash2012 <cyclop9@hotmail.com>
2024-02-27 06:43:22 +0100

    design and implement header and footer sections, import them in App.jsx and render them in the web app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 
 function App() {
   return (
-    <div className="">
-      Yo!
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Ahime</h1>
+        </Container>
+      </main>
+    <Footer/>
+    </>
   );
 }
 
