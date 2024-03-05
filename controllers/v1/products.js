0fdfd736bcbf24cb9232d3d63221ba42b95853f4
YellowFlash2012 <cyclop9@hotmail.com>
2024-03-05 07:32:02 +0100

    refactor the routes by extracting the logic into a different folder called controller

---
+++ controllers/v1/products.js
@@ -0,0 +1,34 @@
+import asyncHandler from "../../middleware/asyncHandler.js";
+import Product from "../../models/Product.js";
+
+// @desc Fetch all products
+// @route GET /api/v1/products
+// @ access Public
+export const getAllProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.status(200).json({
+        message: "success",
+        count: products.length,
+        data: products,
+    });
+})
+
+// @desc Get one product
+// @route GET /api/v1/products/:id
+// @ access Public
+export const getOneProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+        res.status(200).json({
+            message: "success",
+            data: product,
+        });
+
+        return;
+    } else {
+        res.status(404);
+
+        throw new Error("Product NOT found!");
+    }
+})
--- routes/v1/products.js
+++ routes/v1/products.js
@@ -1,37 +1,12 @@
 import express from "express";
-import asyncHandler from "../../middleware/asyncHandler.js";
-import Product from "../../models/Product.js";
+import { getAllProducts, getOneProduct } from "../../controllers/v1/products.js";
+
 
 const router = express.Router();
 
-router.get("/", asyncHandler( async(req, res) => {
-    const products = await Product.find({});
-    res.status(200).json({
-        message:"success",
-        count:products.length,
-        data:products
-    });
-}))
+router.get("/", getAllProducts)
 
-router.get("/:id", asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-        res.status(200).json({
-            message: "success",
-            data: product,
-        });
-
-        return;
-        
-    } else {
-        res.status(404);
-
-        throw new Error("Product NOT found!")
-    }
-        
-    
-}))
+router.get("/:id", getOneProduct)
 
 
 export default router
