17e93511c81bac79070670b48aa16e9be1c522f1
YellowFlash2012 <cyclop9@hotmail.com>
2024-03-13 07:52:32 +0100

    design and implement complete addToCart reducer

--- client/src/features/cartSlice.js
+++ client/src/features/cartSlice.js
@@ -4,12 +4,45 @@ const initialState = localStorage.getItem("cart")
     ? JSON.parse(localStorage.getItem("cart"))
     : { cartItems: [] };
 
+const addDecimals = (num) => {
+    return (
+        Math.round(num*100)/100
+    ).toFixed(2)
+}
+
 const cartSlice = createSlice({
     name: "cart",
     initialState,
-    reducers:{}
+    reducers: {
+        addToCart: (state, action) => {
+            const item = action.payload;
+
+            const existingItem = state.cartItems.find(x => x._id === item._id);
+
+            if (existingItem) {
+                state.cartItems = state.cartItems.map(x => x._id === existingItem._id ? item : x);
+            } else {
+                state.cartItems=[...state.cartItems,item]
+            }
+
+            // calculate item price
+            state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
+
+            // calculate item shipping price (if order>$100 then free else $10)
+            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+            // calculate tax (15% sales tax)
+            state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+
+            // calculate total price
+            state.totalPrice = (Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)).toFixed(2);
+
+            localStorage.setItem("cart", JSON.stringify(state));
+
+        }
+    }
 })
 
-export const { } = cartSlice.actions;
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer
