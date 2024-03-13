be13da54908c88f2986826d79efe3b62ceffd6f2
YellowFlash2012 <cyclop9@hotmail.com>
2024-03-13 07:21:23 +0100

    design and implement an empty shell cartSlice and update store.js accordingly

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,13 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { apiSlice } from '../features/apiSlice';
+import cartReducer from '../features/cartSlice';
 
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]:apiSlice.reducer,
+
+    cart:cartReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools:true
